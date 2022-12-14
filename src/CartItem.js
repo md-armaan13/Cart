42e301f8ffb70cc7d9cf06c6ce89be109b0468e7
MD ARMAAN ANSARI <arman.kanpur4080@gmail.com>
2022-12-14 18:06:53 +0530

    adding state

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,38 +1,91 @@
 import React from 'react';
-
+import './CartItem.css';
 class CartItem extends React.Component{
 
-render(){
+constructor(){
+super();
 
+this.state = {
+
+  price : 99,
+  title : "Shirt",
+  discription : 'Cotton T-Shirt',
+  qty : 1
+
+}
+
+}
+
+increaseQuantity= ()=>{
+  console.log(this.state.qty+1);
+// first form 
+  // this.setState({
+  //   qty : this.state.qty+1,
+  // })
+
+  //second form (you can pass the call back functioin)
+
+  this.setState((prevState)=>{
+    return {
+      qty : prevState.qty+1
+    }
+  })
+
+}
+
+decreaseQuantity= ()=>{
+  console.log(this.state.qty-1);
+// first form 
+  // this.setState({
+  //   qty : this.state.qty+1,
+  // })
+
+  //second form (you can pass the call back functioin)
+
+  this.setState((prevState)=>{
+    return {
+      qty : prevState.qty-1
+    }
+  })
+
+}
+
+
+render(){
+  const { price, title,discription,qty} = this.state;
 return (
 
-    <div className="card mb-3">
-    <div className="card-body">
-      <div className="d-flex justify-content-between">
-        <div className="d-flex flex-row align-items-center">
-          <div>
-          <img
-          src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
-          className="img-fluid rounded-3" alt="Shopping item" style={{width: 65}}/>
-          </div>
-          <div className="ms-3">
-            <h5>Iphone 11 pro</h5>
-            <p className="small mb-0">256GB, Navy Blue</p>
-          </div>
-        </div>
-        <div className="d-flex flex-row align-items-center">
-          <div style={{width : 50}} >
-            <h5 className="fw-normal mb-0">2</h5>
-          </div>
-          <div style={{width : 80}} >
-            <h5 className="mb-0">$900</h5>
-          </div>
-          <a href="#!"  style={{color : "#cecece" }}><i className="fas fa-trash-alt"></i></a>
-        </div>
-      </div>
+    <div className="row mb-4 d-flex justify-content-between align-items-center">
+    <div className="col-md-2 col-lg-2 col-xl-2">
+      <img
+        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img5.webp"
+        className="img-fluid rounded-3" alt="Cotton T-shirt" style={{height : 80 , width : 80} }/>
+    </div>
+    <div className="col-md-3 col-lg-3 col-xl-3">
+      <h6 className="text-muted">{title}</h6>
+      <h6 className="text-black mb-0">{discription}</h6>
+    </div>
+    <div className="col-md-3 col-lg-3 col-xl-2 d-flex">
+      <button className="btn btn-link px-2"
+        onClick={this.decreaseQuantity}>
+        <i className="fas fa-minus"></i>
+      </button>
+
+      <input id="form1" min="0" name="quantity" value= {qty} type="number" 
+        className="form-control form-control-sm" />
+
+      <button className="btn btn-link px-2"
+        onClick={this.increaseQuantity}>
+        <i className="fas fa-plus"></i>
+      </button>
+    </div>
+    <div className="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+      <h6 className="mb-0">€ {price}</h6>
+    </div>
+    <div className="col-md-1 col-lg-1 col-xl-1 text-end">
+      <a href="#!" className="text-muted"><i className="fas fa-times"></i></a>
     </div>
   </div>
-
 );
 
 
